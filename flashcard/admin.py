ecfee6b85a34a0b82c0936393b4313cc5ac35daa
Eduarda Pouzada <150561692+eduardapouzada@users.noreply.github.com>
2024-01-17 20:32:57 -0300

    admin

---
+++ flashcard/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from .models import Categoria, Flashcard
+
+# eduardasuper
+# 1234
+
+admin.site.register(Categoria)
+admin.site.register(Flashcard)
--- usuarios/views.py
+++ usuarios/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 from django.contrib.auth.models import User
 from django.contrib.messages import constants
 from django.contrib import messages
-
+from django.contrib import auth
 def cadastro(request):
     if request.method== 'GET':
         return render(request, 'cadastro.html')
@@ -29,10 +29,36 @@ def cadastro(request):
                 username= username,
                 password= senha
             )    
-            return redirect('/usuarios/login')
+            return redirect('/usuarios/logar')
         
         except:
             messages.add_message(request, constants.ERROR, 'Erro interno no servidor.')
             return redirect('/usuarios/cadastro')
         
   
+  
+def logar(request):
+    if request.method == 'GET':
+        return render(request, 'login.html')  
+    
+    elif request.method == 'POST':
+        
+        username = request.POST.get('username')
+        senha = request.POST.get('senha')
+        
+        user = auth.authenticate(request, username=username, password=senha)
+        
+        if user:
+            auth.login(request, user)
+            messages.add_message(request, constants.SUCCESS, 'Logado!')
+            return redirect('/flashcard/novo_flashcard/')
+        
+        else:
+            messages.add_message(request, constants.ERROR, 'Username ou senha inválidos.')
+            return redirect('/usuarios/logar/')
+        
+        
+def logout(request):
+    auth.logout(request)
+    return redirect('/usuarios/logar')
+        
