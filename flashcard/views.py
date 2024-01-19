1f5dff1addaba30b423c7157c2b6a56aeb1a4983
Eduarda Pouzada <150561692+eduardapouzada@users.noreply.github.com>
2024-01-19 18:11:15 -0300

    ...

--- flashcard/views.py
+++ flashcard/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render, redirect
-
+from .models import Categoria, Flashcard 
 # Create your views here.
 
 def novo_flashcard(request):
     if not request.user.is_authenticated:
         return redirect('/usuarios/logar')
+    
     if request.method == "GET":
-        return render(request, 'novo_flashcard.html')
+        categorias = Categoria.objects.all()
+        dificuldades = Flashcard.DIFICULDADE_CHOICES
+        return render(request, 'novo_flashcard.html', {'categorias': categorias, 'dificuldades': dificuldades})
+    
+    elif request.method == "POST":
+        
