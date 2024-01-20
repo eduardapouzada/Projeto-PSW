9f68a3690870e51102f0a08b3eb557176163c56d
Eduarda Pouzada <150561692+eduardapouzada@users.noreply.github.com>
2024-01-19 23:54:48 -0300

    ....

--- flashcard/views.py
+++ flashcard/views.py
@@ -4,12 +4,46 @@ from .models import Categoria, Flashcard
 
 def novo_flashcard(request):
     if not request.user.is_authenticated:
-        return redirect('/usuarios/logar')
-    
-    if request.method == "GET":
+        return redirect('/usuarios/login')
+
+    if request.method == 'GET':
         categorias = Categoria.objects.all()
         dificuldades = Flashcard.DIFICULDADE_CHOICES
-        return render(request, 'novo_flashcard.html', {'categorias': categorias, 'dificuldades': dificuldades})
-    
-    elif request.method == "POST":
-        
+
+        return render(
+            request,
+            'novo_flashcard.html',
+            {
+                'categorias': categorias,
+                'dificuldades': dificuldades,
+            }
+            
+        )
+    elif request.method == 'POST':
+        pergunta = request.POST.get('pergunta')
+        resposta = request.POST.get('resposta')
+        categoria = request.POST.get('categoria')
+        dificuldade = request.POST.get('dificuldade')
+
+        if len(pergunta.strip()) == 0 or len(resposta.strip()) == 0:
+            messages.add_message(
+                request,
+                constants.ERROR,
+                'Preencha os campos de pergunta e resposta',
+            )
+            return redirect('/flashcard/novo_flashcard')
+
+        flashcard = Flashcard(
+            user=request.user,
+            pergunta=pergunta,
+            resposta=resposta,
+            categoria_id=categoria,
+            dificuldade=dificuldade,
+        )
+
+        flashcard.save()
+
+        messages.add_message(
+            request, constants.SUCCESS, 'Flashcard criado com sucesso'
+        )
+        return redirect('/flashcard/novo_flashcard')
