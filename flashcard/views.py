c5e9fed445738f9c027ff7465c4f3e8a47bed6b9
Eduarda Pouzada <150561692+eduardapouzada@users.noreply.github.com>
2024-01-21 11:29:39 -0300

    ...

--- flashcard/views.py
+++ flashcard/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render, redirect
 from .models import Categoria, Flashcard 
+from django.http import HttpResponse
+from django.contrib.messages import constants 
+from django.contrib import messages
 # Create your views here.
 
 def novo_flashcard(request):
@@ -9,16 +12,16 @@ def novo_flashcard(request):
     if request.method == 'GET':
         categorias = Categoria.objects.all()
         dificuldades = Flashcard.DIFICULDADE_CHOICES
+        flashcards = Flashcard.objects.filter(user = request.user)
 
-        return render(
-            request,
-            'novo_flashcard.html',
-            {
-                'categorias': categorias,
-                'dificuldades': dificuldades,
-            }
-            
-        )
+        categoria_filtrar = request.GET.get('categoria')
+        dificuldade_filtrar = request.GET.get('dificuldade_filtrar')
+        
+        return render(request,'novo_flashcard.html',{'categorias': categorias,
+                                                     'dificuldades': dificuldades,
+                                                     'flashcards': flashcards,})
+    
+    
     elif request.method == 'POST':
         pergunta = request.POST.get('pergunta')
         resposta = request.POST.get('resposta')
@@ -26,43 +29,18 @@ def novo_flashcard(request):
         dificuldade = request.POST.get('dificuldade')
 
         if len(pergunta.strip()) == 0 or len(resposta.strip()) == 0:
-            messages.add_message(
-                request,
-                constants.ERROR,
-                'Preencha os campos de pergunta e resposta',
-            )
+            messages.add_message(request,constants.ERROR,'Preencha os campos de pergunta e resposta',)
             return redirect('/flashcard/novo_flashcard')
 
         flashcard = Flashcard(
-            user=request.user,
-            pergunta=pergunta,
-            resposta=resposta,
-            categoria_id=categoria,
-            dificuldade=dificuldade,
+            user = request.user, 
+            pergunta = pergunta,
+            resposta = resposta,
+            categoria_id= categoria,
+            dificuldade = dificuldade,
         )
-
+        
         flashcard.save()
-
-        messages.add_message(
-            request, constants.SUCCESS, 'Flashcard criado com sucesso'
-        )
+        messages.add_message(request,constants.SUCCESS,'Flashcard cadastrado com sucesso.',)
         return redirect('/flashcard/novo_flashcard')
     
-    
-    
-    
-    if request.method == 'GET':
-        categorias = Categoria.objects.all()
-        dificuldades = Flashcard.DIFICULDADE_CHOICES
-        flashcards = Flashcard.objects.filter(user=request.user)
-
-        return render(
-            request,
-            'novo_flashcard.html',
-            {
-                'categorias': categorias,
-                'dificuldades': dificuldades,
-                'flashcards': flashcards,
-            }
-            
-        )
